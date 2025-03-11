65efd713e70b42474681d0563e821e16e4664fb1
Pizzy-23 <parzival1.2323@gmail.com>
2025-03-11 18:09:34 -0300

    fix slimeread

--- src/web/mjs/connectors/Slimeread.mjs
+++ src/web/mjs/connectors/Slimeread.mjs
@@ -1,71 +1,78 @@
-import Connector from '../engine/Connector.mjs';
-import Manga from '../engine/Manga.mjs';
+import Connector from "../engine/Connector.mjs";
+import Manga from "../engine/Manga.mjs";
 
 export default class SlimeRead extends Connector {
+  constructor() {
+    super();
+    super.id = "slimeread";
+    super.label = "SlimeRead";
+    this.tags = ["manga", "portuguese"];
+    this.url = "https://slimeread.com";
 
+    // Definir os caminhos de categorias
+    this.categories = ["mangas", "manhwas", "manhuas", "novels"];
+  }
 
-    constructor() {
-        super();
-        super.id = 'slimeread';
-        super.label = 'SlimeRead';
-        this.tags = ['manga', 'portuguese'];
-        this.baseURL = 'https://slimeread.com';
-        this.language = 'pt-br';
+  async _getMangas() {
+    let mangaList = [];
+    for (const category of this.categories) {
+      const mangas = await this._getMangasFromCategory(category);
+      mangaList.push(...mangas);
     }
+    return mangaList;
+  }
 
-    async _getMangaFromURI(uri) {
-        let parts = uri.pathname.split('/');
-        let id = parts[2];
-        let request = new Request(`${this.baseURL}/manga/${id}`);
-        let data = await this._requestJSON(request.url);
-        let title = data.title;
-        return new Manga(this, id, title);
-    }
+  async _getMangasFromCategory(category) {
+    let mangas = [];
+    let page = 1;
+    let hasNextPage = true;
 
-    _getMangaList(callback) {
-        this._requestJSON(`${this.baseURL}/mangas`)
-            .then(data => {
-                let mangaList = data.map(item => ({
-                    id: item.id,
-                    title: item.title
-                }));
-                callback(null, mangaList);
-            })
-            .catch(error => {
-                console.error(error, this);
-                callback(error, undefined);
-            });
-    }
+    while (hasNextPage) {
+      const uri = new URL(`/${category}?page=${page}`, this.url);
+      console.log("Fetching from:", uri.href);
 
-    _getChapterList(manga, callback) {
-        let url = `${this.baseURL}/ler/${manga.id}`;
-        this.fetchDOM(url, 'a[href*="/ler/"]')
-            .then(elements => {
-                let chapterList = elements.map(el => {
-                    return {
-                        id: el.href.split('/').pop(),
-                        title: el.textContent.trim(),
-                        language: this.language
-                    };
-                });
-                callback(null, chapterList);
-            })
-            .catch(error => {
-                console.error(error, manga);
-                callback(error, undefined);
-            });
-    }
+      const request = new Request(uri.href, this.requestOptions);
+      const dom = await this.fetchDOM(request, "div.comic-card > a");
 
-    _getPageList(manga, chapter, callback) {
-        let url = `${this.baseURL}/ler/${manga.id}/${chapter.id}`;
-        this.fetchDOM(url, 'img[class*="tw-zba"]')
-            .then(elements => {
-                let pageList = elements.map(img => img.src);
-                callback(null, pageList);
-            })
-            .catch(error => {
-                console.error(error, chapter);
-                callback(error, undefined);
-            });
+      if (dom.length === 0) {
+        hasNextPage = false;
+        break;
+      }
+
+      dom.forEach((element) => {
+        mangas.push({
+          id: element.pathname.split("/")[2],
+          title: element.text.trim(),
+        });
+      });
+
+      page++;
+      await this.wait(250);
     }
+    return mangas;
+  }
+
+  async _getChapters(manga) {
+    const uri = new URL(`/manga/${manga.id}`, this.url);
+    console.log("Fetching chapters from:", uri.href);
+
+    const request = new Request(uri.href, this.requestOptions);
+    const dom = await this.fetchDOM(request, "a[href*='/ler/']");
+
+    return dom.map((element) => ({
+      id: element.href.split("/").pop(),
+      title: element.text.trim(),
+      language: this.language,
+    }));
+  }
+
+  async _getPages(chapter) {
+    const uri = new URL(`/ler/${chapter.manga.id}/${chapter.id}`, this.url);
+    console.log("Fetching pages from:", uri.href);
+
+    const request = new Request(uri.href, this.requestOptions);
+    const dom = await this.fetchDOM(request, "img[class*='tw-zba']");
+
+    return dom.map((img) => img.src);
+  }
 }
